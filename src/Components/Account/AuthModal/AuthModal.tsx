38099611ac068751c2dc89515f7d3c1d14e82139
Chimed-ochir <chimed-ochir@infinite.mn>
2023-12-13 15:41:15 +0800

    login input repaire

--- src/Components/Account/AuthModal/AuthModal.tsx
+++ src/Components/Account/AuthModal/AuthModal.tsx
@@ -1,41 +1,27 @@
 import {
-  Box,
-  Divider,
   Button,
   Stack,
   useModalContext,
-  HStack,
-  Link,
   InputRightElement,
   Text,
 } from "@chakra-ui/react";
-// import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
-// import { AiFillEye } from '@react-icons/all-files/ai/AiFillEye';
-// import { AiFillEyeInvisible } from '@react-icons/all-files/ai/AiFillEyeInvisible';
-
 import { PropsWithChildren, useMemo, useState } from "react";
 import { Form, Formik } from "formik";
 import { Modal } from "@/Components/Modal";
 import { FormInput } from "@/Components/form/FormInput";
 import { BuildLoginFormValidationSchema } from "./AuthModal.schema";
-// import { useLogin } from "@/services/auth/login.service";
-// import { useAuthContext } from "@/contexts/AuthContext";
-// import { useConfirmWithResend } from "@/services/auth/confirm.service";
 import { useToast } from "@/utils/toast";
 import localFont from "next/font/local";
 import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
-import { HiOutlineUserCircle } from "react-icons/hi";
 import { useAuth } from "..";
 import { useMutation } from "@/utils";
 import { LoginDataType } from "..";
 import { MfaModal } from "../MfaModal";
-// const LoginFormBody = ({ isLoading }: { isLoading: boolean }) => {
 const satFont = localFont({
   src: "../../fonts/satoshi/Fonts/Variable/Satoshi-Variable.ttf",
 });
 const LoginFormBody = ({ loading }: { loading: boolean }) => {
   const [type, setType] = useState("password");
-  //   const { openLogin, seeLogin } = useAuthContext();
   const changePassword = () => {
     if (type === "password") {
       setType("text");
@@ -73,8 +59,8 @@ const LoginFormBody = ({ loading }: { loading: boolean }) => {
         name="email"
         placeholder={"Нэвтрэх нэр оруулах"}
         color={"white"}
-        // mt={"5px"}
-        // isLoading={isLoading}
+        mt={"5px"}
+        isLoading={loading}
       />
 
       <FormInput
@@ -84,9 +70,8 @@ const LoginFormBody = ({ loading }: { loading: boolean }) => {
         type={type}
         autoComplete="current-password"
         color={"white"}
-        // mt={"5px"}
-        //   isLoading={isLoading}
-
+        mt={"5px"}
+        isLoading={loading}
         inputRightElement={
           <InputRightElement onClick={changePassword} cursor={"pointer"}>
             {type === "password" ? (
@@ -97,8 +82,6 @@ const LoginFormBody = ({ loading }: { loading: boolean }) => {
           </InputRightElement>
         }
       />
-
-      {/* <Button variant="white" size="sm" type="submit" isLoading={isLoading}> */}
 
       <Button
         type="submit"
@@ -125,41 +108,6 @@ const LoginForm = () => {
   const [mfa, setMfa] = useState(false);
   const [ner, setNer] = useState("");
   let spell: any = "bat";
-  //   const { confirmWithResend } = useConfirmWithResend(
-  //     (data) => {
-  //       if (data?.$metadata.httpStatusCode === 200) {
-  //         showSuccessToast(
-  //           `${t("authTokenModal.fields.email")} ${
-  //             data?.CodeDeliveryDetails.Destination
-  //           }` as string
-  //         );
-  //       }
-
-  //       // onFinish(data);
-  //     },
-  //     (error) => showErrorToast(error?.message)
-  //   );
-  //   const { logins, isLoginLoading } = useLogin(
-  //     (data: any, values: any) => {
-  //       if (data?.ChallengeName === "SOFTWARE_TOKEN_MFA") {
-  //         setSessionData({
-  //           Session: data?.Session,
-  //           username: values.username,
-  //           sub: data?.sub,
-  //         });
-  //         return;
-  //       }
-  //       onFinish(data);
-  //     },
-  //     (error: any) => {
-  //       if (error?.message === "UserNotConfirmedException") {
-  //         confirmWithResend(spell);
-  //         setIsOp(true);
-  //       } else {
-  //         showErrorToast("Nevtrehed aldaa garlaa");
-  //       }
-  //     }
-  //   );
   const { loading, request } = useMutation({
     uri: `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/login`,
   });
@@ -182,18 +130,8 @@ const LoginForm = () => {
       });
   };
 
-  // login(res);
-  // console.log("<<<<>>>>", values);
-
-  // console.log("---", res);
-
   const onFinish = (data: any) => {
-    // const authenticationResult = login(data);
     login(data);
-    // if (authenticationResult) {
-    //   showSuccessToast("Amjiltai nevterlee");
-    //   onClose();
-    // }
   };
   return (
     <Stack>
