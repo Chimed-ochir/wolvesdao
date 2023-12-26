a517e40bb2725b7898613fe3caf68653ad6e58a1
Munkhsaikhan <84602659+cryptonomad2021@users.noreply.github.com>
2023-12-26 17:54:52 +0800

    done

--- src/app/rules/faq/page.tsx
+++ src/app/rules/faq/page.tsx
@@ -2,9 +2,11 @@ import { Stack, Text } from "@chakra-ui/react";
 import React from "react";
 import Faq from "@/Components/Faqrule/page";
 import localFont from "next/font/local";
+
 const satFont = localFont({
   src: "../../../fonts/satoshi/Fonts/Variable/Satoshi-Variable.ttf",
 });
+
 function page() {
   return (
     <Stack
@@ -15,7 +17,8 @@ function page() {
       <Text
         {...satFont.style}
         lineHeight={"32px"}
-        fontSize={{ base: "16px", sm: "24px" }}
+        fontSize={{ base: "24px", sm: "24px" }}
+        ml={["12px", "0"]}
         fontWeight={"900"}
         color={"#F2F2F2"}
       >
