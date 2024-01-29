8964dd3dc8db53a49bafe3753341322adb5c67e7
Saikhanbileg21 <saikhnaa@mongolnft.com>
2024-01-29 11:07:30 +0800

    update

--- src/Components/Account/UpdateModal/UpdateModal.tsx
+++ src/Components/Account/UpdateModal/UpdateModal.tsx
@@ -169,14 +169,12 @@ const UpdateForm = ({
           content: data?.data?.content,
           description: data?.data?.description,
 
-          endDate: moment
-            .utc(data?.data?.endDate)
+          endDate: moment(data?.data?.endDate)
             .format("YYYY-MM-DDTHH:mm")
             .slice(0, 16),
           listContent: data?.data?.listContent,
           options: data?.data?.options,
-          startDate: moment
-            .utc(data?.data?.startDate)
+          startDate: moment(data?.data?.startDate)
             .format("YYYY-MM-DDTHH:mm")
             .slice(0, 16),
           withLink: data?.data?.withLink,
