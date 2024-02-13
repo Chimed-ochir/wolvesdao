768c908408fcee99628383aba977fb57cf57a9e8
Ds21saikhnaa <dashmandalsaikhanbileg@gmail.com>
2024-02-13 15:14:51 +0800

    da

--- src/app/voting/[id]/page.tsx
+++ src/app/voting/[id]/page.tsx
@@ -55,9 +55,15 @@ function Page({ params: { id } }: { params: { id: string } }) {
   const [cont1, setCont1] = useState(false);
   const toast = useToast();
   useEffect(() => {
-    if (data?.data?.meVotedId) {
-      setSend(data?.data?.meVotedId as string);
-      setFirst(data?.data?.meVotedId as string);
+    // console.log(data.data)
+    // if (data?.meVotedId) {
+    //   setSend(data?.meVotedId as string);
+    //   setFirst(data?.meVotedId as string);
+    //   setVoteId(false);
+    // }
+    if (data && "meVotedId" in data) {
+      setSend(data?.meVotedId as string);
+      setFirst(data?.meVotedId as string);
       setVoteId(false);
     }
   }, [data]);
